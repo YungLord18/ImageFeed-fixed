39d2e3507a05c9d2249e698a9602d12810ad1cc8
YungLord <marikoryagina.den@yandex.ru>
2024-05-15 18:43:43 +0300

    Исправил ошибки с файлами

---
+++ ImageFeedTests/ImageFeedTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+@testable import ImageFeed
+
+final class ImageFeedTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testExample() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        
+        self.measure {
+        
+        }
+    }
+
+}
