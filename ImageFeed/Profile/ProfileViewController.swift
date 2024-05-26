63e3ebc178bce038efb74502d17ecf584c57ac4a
YungLord <marikoryagina.den@yandex.ru>
2024-05-26 17:03:36 +0300

    Сделал самостоятельную работу с настройкой экрана профиля

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+final class ProfileViewController {
+    
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var avatarImageView: UIImageView!
+    
+    @IBOutlet private var logoutButton: UIButton!
+    
+    
+    @IBAction private func didTapLogoutButton() {
+    }
+}
