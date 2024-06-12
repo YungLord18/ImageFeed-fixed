9d2a626714e150c0602243ff02e10403776b24d4
YungLord <marikoryagina.den@yandex.ru>
2024-06-12 17:12:45 +0300

    Проверка перед сдачей 10 спринта

---
+++ ImageFeed/ImagesList/Constants.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+let accessKey = "Mg0HD6jMC4P_I0FZTVWC68P7VP5XOhQfP_OvlzjrJsk"
+let secretKey = "UI6IrAmgZh7idh7fsGJN2ObuVZxo1CCVG-0GNZrYHfU"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let accessScope = "public+read_user+write_likes"
+let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+
